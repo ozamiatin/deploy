f753facd369db426244d324da4e3cabd5e6ad7de
ozamiatin <ozamiatin@mirantis.com>
2016-02-18 16:48:43 +0200

    Fix read-only clone

--- get_patch.sh
+++ get_patch.sh
@@ -1,8 +1,10 @@
 # $1 - change-id
 # $2 - directory
+# $3 - read-only checkout (https)
 
 CHANGE_ID=$1
 WORK_DIRECTORY=$2
+READ_ONLY=$3
 
 mkdir "$WORK_DIRECTORY"
 
@@ -18,8 +20,14 @@ if [ ! -d "py27" ]; then
 	echo "py27 env was not created!"
 	exit 1
 fi
-	
-git clone ssh://ozamiatin@review.openstack.org:29418/openstack/oslo.messaging
+
+if [[ $READ_ONLY -eq "--read-only" ]] ; then
+	echo "Cloning HTTPS"
+	git clone https://review.openstack.org/openstack/oslo.messaging
+else
+	echo "Cloning SSH"
+	git clone ssh://ozamiatin@review.openstack.org:29418/openstack/oslo.messaging
+fi
 
 cd ./oslo.messaging || exit 1
 git review -d $CHANGE_ID
